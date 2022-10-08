e055b4102e66a3aed5bb316e1239553b6342449c
Kenji Rikitake <kenji.rikitake@acm.org>
2022-10-09 01:28:35 +0900

    util.go: add bStrictToLower() and use them
    * To preserve non-ASCII encoding in the field data,
      which is not necessarily in UTF-8 and may cause length changes.
      See bytes.ToLower() source code.

--- util.go
+++ util.go
@@ -4,16 +4,31 @@ import (
 	"bytes"
 )
 
+// Strictly-ASCII-only lowercase converter
+// No Unicode processing
+// See bytes.ToLower() source code
+func bStrictToLower(s []byte) []byte {
+	b := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if 'A' <= c && c <= 'Z' {
+			c += 'a' - 'A'
+		}
+		b[i] = c
+	}
+	return b
+}
+
 // Case-insensitive bytes.Index
-// This function may break when handling non-ASCII characters
+// This function only handles ASCII bytes - no Unicode-specific conversion
 func bIndexCI(b, subslice []byte) int {
-	return bytes.Index(bytes.ToLower(b), bytes.ToLower(subslice))
+	return bytes.Index(bStrictToLower(b), bStrictToLower(subslice))
 }
 
 // Case-insensitive bytes.Contains
-// This function may break when handling non-ASCII characters
+// This function only handles ASCII bytes - no Unicode-specific conversion
 func bContainsCI(b, subslice []byte) bool {
-	return bytes.Contains(bytes.ToLower(b), bytes.ToLower(subslice))
+	return bytes.Contains(bStrictToLower(b), bStrictToLower(subslice))
 }
 
 // Find start of next tag
